16a94cd14e1acc6a5be6365664758d9af67ae94c
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-03-08 05:17:22 -0800

    Update yt_thumb_dl.py

--- plugins/josprojects/yt_thumb_dl.py
+++ plugins/josprojects/yt_thumb_dl.py
@@ -40,13 +40,13 @@ async def send_thumbnail(bot, update):
         )
         await update.reply_photo(
             photo=thumbnail,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='https://telegram.me/josprojects')]]),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='https://telegram.me/slogan_98')]]),
             quote=True
         )
         await message.delete()
     except Exception as error:
         await message.edit_text(
-            text="**Please Use** /ytthumb (youtube link)\n\n**Example:** `/ytthumb https://youtu.be/h6PtzFYaMxQ`",
+            text="**Please Use** /ytthumb (youtube link)",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='https://telegram.me/josprojects')]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='ttps://telegram.me/slogan_98')]])
         )
