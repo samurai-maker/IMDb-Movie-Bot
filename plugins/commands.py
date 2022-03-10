a070fe8087916fcde6d840f38af728e5902529ec
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-03-10 07:23:07 -0800

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,7 +31,7 @@ async def start(client, message):
             InlineKeyboardButton('Close ✗', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await client.send_sticker(chat_id=message.chat.id, reply_markup=reply_markup, reply_to_message_id=message.message_id)
+        await client.send_sticker(chat_id=message.chat.id,sticker='CAACAgIAAxkBAAPrYioXVaTWzaVY_ZB1TttMYK0rkcsAAq0WAAIdY1BJPOls03MkWaweBA', reply_markup=reply_markup, reply_to_message_id=message.message_id)
         await asyncio.sleep(60)
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
